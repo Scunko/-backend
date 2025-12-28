ec290ee0c2c7dd191e97ebb77c80a0177eb8eab2
Michael <88518184+Scunko@users.noreply.github.com>
2025-12-28 13:58:11 -0800

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Backend is working" });
+  res.status(200).json({
+    method: req.method,
+    message: "Backend received your request",
+    query: req.query
+  });
 }
